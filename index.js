292d4bfb73b55e289ebb83df3b8cd4b4a177663f
Navdeep Kumar Mandal <47221459+Navdeepmn@users.noreply.github.com>
2020-06-20 12:19:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src", randomDiceImage1);
+var randomNumber2 = Math.random();
+randomNumber2= randomNumber2*6;
+randomNumber2= Math.floor(randomNumber2)+1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+var image1 = document.querySelector(".img2");
+image1.setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
